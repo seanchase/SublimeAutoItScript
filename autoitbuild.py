8a29f3b5f096188b98921cd4ab7a4e6f623f6093
robertcollier4 <robertcollier4@yahoo.com>
2013-08-15 19:45:10 +0530

    Added AZJIO IncludeHelper

--- autoitbuild.py
+++ autoitbuild.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
 import sublime, sublime_plugin
 import subprocess
+import os
 
 # The autoitbuild command is called as target by AutoIt.sublime-build
 class autoitbuild(sublime_plugin.WindowCommand):
-
 	def run(self):
 		filepath = self.window.active_view().file_name()
 		AutoItExePath = sublime.load_settings("AutoIt.sublime-settings").get("AutoItExePath")
@@ -12,7 +12,6 @@ class autoitbuild(sublime_plugin.WindowCommand):
 		self.window.run_command("exec", {"cmd": cmd})
 
 class autoitcompile(sublime_plugin.WindowCommand):
-
 	def run(self):
 		filepath = self.window.active_view().file_name()
 		AutoItCompilerPath = sublime.load_settings("AutoIt.sublime-settings").get("AutoItCompilerPath")
@@ -20,7 +19,6 @@ class autoitcompile(sublime_plugin.WindowCommand):
 		self.window.run_command("exec", {"cmd": cmd})
 
 class autoittidy(sublime_plugin.WindowCommand):
-
 	def run(self):
 		self.window.run_command("save")
 		filepath = self.window.active_view().file_name()
@@ -44,3 +42,28 @@ class autoittidy(sublime_plugin.WindowCommand):
 			print("TidyCmd was: " + " ".join(tidycmd))
 			print("Error {0}".format(str(e)))
 			sublime.status_message("### EXCEPTION: " + str(e))
+
+class autoitincludehelper(sublime_plugin.WindowCommand):
+	def run(self):
+		self.window.run_command("save")
+
+		filepath = self.window.active_view().file_name()
+		AutoItExePath = sublime.load_settings("AutoIt.sublime-settings").get("AutoItExePath")
+		AutoItIncludeFolder = os.path.dirname(AutoItExePath) + "\\Include"
+
+		IncludeHelperAU3Path = sublime.load_settings("AutoIt.sublime-settings").get("IncludeHelperAU3Path")
+		if (IncludeHelperAU3Path is None):
+			IncludeHelperAU3Path = "{PACKAGE_PATH}\\AutoItScript\\Include_Helper.au3"
+		IncludeHelperAU3Path = IncludeHelperAU3Path.replace("{PACKAGE_PATH}", sublime.packages_path())
+
+		AutoItIncludeCmd = [AutoItExePath, IncludeHelperAU3Path, filepath, AutoItIncludeFolder]
+
+		try:
+			subprocess.call(AutoItIncludeCmd)
+			self.window.run_command("revert")
+			sublime.status_message("AutoIt IncludeHelper Finished")
+		except Exception as e:
+			sublime.active_window().run_command("show_panel", {"panel": "console"})
+			print("------------ ERROR: Python exception trying to run following command ------------")
+			print(AutoItIncludeCmd)
+			print("Error {0}".format(str(e)))
